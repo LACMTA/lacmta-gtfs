5baeee9dc9c44c67a660cc838fa5ae08b01f6499
albertkun <albertk@gmx.com>
2021-12-06 17:45:38 -0800

    feat: hacky solution for push to github

--- update_gtfs.py
+++ update_gtfs.py
@@ -5,6 +5,7 @@ import re
 USER = os.environ.get('FTP_USERNAME')
 PASS = os.environ.get('FTP_PASS')
 SERVER = os.environ.get('SERVER')
+WORKSPACE = os.environ.get('GITHUB_WORKSPACE')
 REMOTEPATH = '/nextbus/prod/'
 
 ftp = ftplib.FTP(SERVER)
@@ -19,5 +20,13 @@ os.chdir("data/")
 for filename in ftp.nlst(filematch): # Loop - looking for matching files
     fhandle = open(filename, 'wb')
     print('Getting ' + filename) #for confort sake, shows the file that's being retrieved
-    ftp.retrbinary('RETR ' + filename, fhandle.write)
+    the_file = ftp.retrbinary('RETR ' + filename, fhandle.write)
     fhandle.close()
+
+push_to_github()
+    
+def push_to_github():
+    os.system('git pull')
+    os.system('git add .')
+    os.system('git commit -m "Auto update"')
+    os.system('git push')
