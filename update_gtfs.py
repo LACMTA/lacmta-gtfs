5d7e1f8222eef07da43ecfcab9ce62a38addf2b2
albertkun <albertk@gmx.com>
2021-12-06 17:06:22 -0800

    feat: initial scripts/notebook for automation

---
+++ update_gtfs.py
@@ -0,0 +1,23 @@
+import ftplib
+import os
+import re
+
+USER = os.environ.get('FTP_USERNAME')
+PASS = os.environ.get('FTP_PASS')
+SERVER = os.environ.get('SERVER')
+REMOTEPATH = '/nextbus/prod/'
+
+ftp = ftplib.FTP(SERVER)
+ftp.login(USER, PASS)
+
+directory = REMOTEPATH
+filematch = '*.txt'
+ftp.cwd(directory)
+ftp.retrlines("LIST")
+os.chdir("data/")
+
+for filename in ftp.nlst(filematch): # Loop - looking for matching files
+    fhandle = open(filename, 'wb')
+    print('Getting ' + filename) #for confort sake, shows the file that's being retrieved
+    ftp.retrbinary('RETR ' + filename, fhandle.write)
+    fhandle.close()
